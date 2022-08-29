161ee600962e0c868d18b836070b2149f7f3e6c5
Fityan <fityannugroho@gmail.com>
2022-08-29 17:58:37 +0700

    Initial NestJS

---
+++ README.md
@@ -0,0 +1,1 @@
+NestJS boilerplate using Fastify
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  // Start the server
+  await app.listen(3000);
+  console.log(`Server running on ${await app.getUrl()}`);
+}
+bootstrap();
