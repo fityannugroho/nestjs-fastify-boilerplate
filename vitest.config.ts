35d156a038da15e53a5c4e2933c0e68c0a8535fb
Fityan <63894003+fityannugroho@users.noreply.github.com>
2026-02-04 20:45:20 +0700

    chore: migrate test tools from Jest to Vitest

---
+++ vitest.config.ts
@@ -0,0 +1,27 @@
+import swc from 'unplugin-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    root: './',
+    environment: 'node',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov', 'html'],
+      reportsDirectory: './coverage',
+    },
+  },
+  plugins: [
+    swc.vite({
+      jsc: {
+        transform: {
+          legacyDecorator: true,
+          decoratorMetadata: true,
+        },
+      },
+    }),
+    tsconfigPaths(),
+  ],
+});
